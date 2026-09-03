a96795803c4430c2a3752461d08b3ee9c76849ae
barnakolos01-alt <barnakolos01@gmail.com>
2026-09-03 09:10:25 +0200

    Create rescueOptions.js

---
+++ src/utils/rescueOptions.js
@@ -0,0 +1,14 @@
+// ---------------------------------------------------------------------------
+// Esti mentőöv opciók
+// ---------------------------------------------------------------------------
+export const RESCUE_OPTIONS = [
+  {
+    title: "150g zsírszegény túró fahéjjal és édesítővel (lassú felszívódás éjszakára)",
+    delta: { protein: 1 },
+  },
+  { title: "1 db főtt tojás vagy 2 szelet sonka kígyóuborkával", delta: { protein: 1 } },
+  {
+    title: "Meleg mandulatej egy csipet fahéjjal és 8-10 szem mandulával",
+    delta: { fat: 0.5 },
+  },
+];
