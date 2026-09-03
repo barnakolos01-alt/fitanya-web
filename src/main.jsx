750593b5089e790609ba87160c47a3e39ef6de45
barnakolos01-alt <barnakolos01@gmail.com>
2026-09-03 09:24:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// Service Worker regisztrálása a PWA működéshez
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
